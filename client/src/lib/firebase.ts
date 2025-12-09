f9e5e8d469af7eaec9ceab9cf4765388b4cd8f89
sedrickwall <sedrick.wall.jr@gmail.com>
2025-12-09 14:03:09 +0000

    Firebase updates

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -1,4 +1,3 @@
-// client/src/lib/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -9,11 +8,11 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+console.log("FIREBASE CONFIG:", firebaseConfig); // TEMP debug
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
